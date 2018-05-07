2383175761470a7eee3650a3846ff756492addd3
Qidian213 <xhx1247786632@gmail.com>
2018-05-07 17:00:06 +0800

    yolov3_deep_sort

---
+++ README.md
@@ -0,0 +1,42 @@
+# keras-yolo3
+
+[![license](https://img.shields.io/github/license/mashape/apistatus.svg)](LICENSE)
+
+## Introduction
+
+  https://github.com/nwojke/deep_sort
+  https://github.com/qqwweee/keras-yolo3
+
+## Quick Start
+
+1. Download YOLOv3 weights from [YOLO website](http://pjreddie.com/darknet/yolo/).
+2. Convert the Darknet YOLO model to a Keras model.
+3. Run YOLO_DEEP_SORT 
+
+```
+   wget https://pjreddie.com/media/files/yolov3.weights
+   python convert.py yolov3.cfg yolov3.weights model_data/yolo.h5
+   python demo.py
+```
+
+#Dependencies
+
+The code is compatible with Python 2.7 and 3. The following dependencies are needed to run the tracker:
+
+    NumPy
+    sklean
+    OpenCV
+
+Additionally, feature generation requires TensorFlow-1.4.0.
+
+#Note 
+ file model_data/mars-small128.pb  had convert to tensorflow-1.4.0
+ file model_data/yolo.h5 is to large to upload ,so you need convert it from Darknet Yolo model to a keras model by yourself
+ 
+# speed
+ when only run yolo detection about 11-13 fps
+ after add deep_sort about 11.5 fps
+ 
+
+
+
---
+++ demo.py
@@ -0,0 +1,80 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import division, print_function, absolute_import
+
+import os
+from timeit import time
+import warnings
+import sys
+import cv2
+import numpy as np
+from PIL import Image
+from yolo import YOLO
+
+from deep_sort import preprocessing
+from deep_sort import nn_matching
+from deep_sort.detection import Detection
+from deep_sort.tracker import Tracker
+from tools import generate_detections as gdet
+from deep_sort.detection import Detection as ddet
+warnings.filterwarnings('ignore')
+
+def main(yolo):
+
+   # Definition of the parameters
+    max_cosine_distance = 0.05
+    nn_budget = None
+    nms_max_overlap = 1.0
+    
+   # deep_sort 
+    model_filename = 'model_data/mars-small128.pb'
+    encoder = gdet.create_box_encoder(model_filename,batch_size=1)
+    
+    metric = nn_matching.NearestNeighborDistanceMetric("cosine", max_cosine_distance, nn_budget)
+    tracker = Tracker(metric)
+
+    video_capture = cv2.VideoCapture(0)
+    fps = 0.0
+    while True:
+        _, frame = video_capture.read()  # frame shape 640*480*3
+        t1 = time.time()
+
+        image = Image.fromarray(frame)
+        boxs = yolo.detect_image(image)
+        
+        features = encoder(frame,boxs)
+        
+        # score to 1.0 here).
+        detections = [Detection(bbox, 1.0, feature) for bbox, feature in zip(boxs, features)]
+        
+        # Run non-maxima suppression.
+        boxes = np.array([d.tlwh for d in detections])
+        scores = np.array([d.confidence for d in detections])
+        indices = preprocessing.non_max_suppression(boxes, nms_max_overlap, scores)
+        detections = [detections[i] for i in indices]
+        
+        # Call the tracker
+        tracker.predict()
+        tracker.update(detections)
+        
+        for track,det in zip(tracker.tracks,detections):
+            bbox = track.to_tlbr()
+            cv2.rectangle(frame, (int(bbox[0]), int(bbox[1])), (int(bbox[2]), int(bbox[3])),(255,255,255), 4)
+            bbox = det.to_tlbr()
+            cv2.rectangle(frame,(int(bbox[0]), int(bbox[1])), (int(bbox[2]), int(bbox[3])),(255,0,0), 4)
+            cv2.putText(frame, str(track.track_id),(int(bbox[0]), int(bbox[1])),0, 5e-3 * 480, (124,252,0),4)
+        cv2.imshow('', frame)
+        
+        fps  = ( fps + (1./(time.time()-t1)) ) / 2
+        print("fps= %f"%(fps))
+        
+        # Press Q to stop!
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+          break
+
+    video_capture.release()
+    cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    main(YOLO())
